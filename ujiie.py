e43ff0757e9b75d081b5fec8c256f1ceffc94082
Shogo Ujiie <shogo-ujiie@m3.com>
2024-11-02 10:07:32 +0900

    add m3 quine

---
+++ ujiie.py
@@ -0,0 +1,30 @@
+exec("""import_re;m=lambda_x:re.sub("~{2}",chr(32),"".join(x.split()))""".replace("_",chr(32)),globals());
+exec(s:=m('''s="""exec(\\"\\"\\"import_re;m=lambda_x:re.sub("~{2}",chr(32),"".join(x.split()))\\"\\"\\".re
+place("_",chr(32)),globals());exec(s:=m(\'\'%r\'\'))"""%s;import~~base64~~as~~b6;import~~zlib;import~~rand
+om;from~          ~datetime~~import~~datetime;          random.seed(dateti                me.now().timesta
+mp());tr          =lambda~~x,y,z:~~(y~~if~~x~~          else~~z);lme=l                        ambda~~x:lis
+t(map(la            mbda~~x:tr(x=="1",1,0),x))          ;di=zlib.dec            ompr            ess(b6.b64
+decode("            7ZtRktwgDET/cxpy/8ulUpWs            QaglgVuM8aCv      XdTwhAdcdDHz6/        ey+PVuVPkJ
+Sz6iWoBC            CgUVGQZprFGGUJ6mlEWoKxNC            IZmvqEtIRv1tl8o0VK90UPYDwvmno3SpnS      WivEJ2QI38
+d1Agff2P      M/      uh2heRPxABhUvYE4VOCe      9A      xQYaQOXHQR2UhdLfDDKwqohIQklMaAt7ME      0je6JxCPZA
+66ONRDU9      vd      ZAcc7sX41i2IMnou47EV      2T      aA8WoHRQwpkdgdJQWoaMsnJUe2DPl4jylg      vNiXggmj3w
+QSRUBESx      B8      WMp6Lkg7iNyo+DOigLpS      3P      PiwVXuZUlL+rSHcisq+zhSMopLHmSUZZao      jindm/FsWy
+B8tQ2uIh      OpHS      RZsn2gMLk4oKPsA7      9kBi      01E1bgFKKlMuKp6LGnUiBBQuIQZaiLr+J9      oDvcwUlBZ1
+P8qdiI9p      UPlx      UAdlofAirSOm0rdJ      4hfB      AqhxJxLT0O0B1nwOdffMbmmoqCvzNN        S8PajbU1Ftax
+A150Tath      uoSCFL      UJEWypk9MsvdUH    iRBFFx      e1DceBIq6kSIKFyCFZ                  rmGaioPdgTFRlm
+CJUfB3VQ      Fkp/M8      hAqv7N5+6rOZ      QOkir6      nQgJhTT+0/gsau7Mfl              CNXO9o5T+AspcVxYkU
+NSZRkawH      S/kimP      4o024Pelj6nc      iTUHF7      sBcq6kSIKFxCBERG2S                    /nECpqD6QKlZ
+h6J9L2S7      sTkb1ef7      o9qH1Q1j      K9wlfhRU      9F2fuLeCfijUC7qPDLJKMwiHZ74IO+          BrWJE7n+1k
+YgOhFtC0      +gItl6eA      1GRmmtJm      r0zI4+nT      SUBH0A9T9DsQcI1WZSUaEHWLf4A5NQsYF1        WCJqal/h
+AewZ9H7k      UhDvRIoS      dZ56J2KB      Xn+6Pah9      UGiJtgFV2qYSYg7KBv2Tc5xIAJT6442qywKU      NavZM7s/
+p9eghDDx      dyLrUJ2M+D      sREs      orxAd9HjV6      ZgeghSjRmIFS55RzUQEEfBSYU8adCEzz70SW      oYwk+ytT
+y1BwSQRR      A4WbE3756f      ag9k      HpbwYZrqo0      kYYqalioCSeipTopzx5ozSkovblpZZ3ZUbe9      UeOfFS7Q
+yyuflmhh      OpF26A7NdSKl      iy    rJtgcQxEaNz2      ryzD7zWU2h0KOwVuAUCk21amfZg01Qg06Egc      IlPAvVdO
+fZg14sWv      goGT+CDCcCnv            zLT7cHtQ9Kfz      PIcFRgmbNR5vYFqCkn0ibsLRxBGZq+sRNy        UHo3GzV5
+ZkedmvYv      QU3Zgw1QE06E            iXIKKQVVR0f1      sFKFhbphD1pEEkrCOmni70SCqGF78GGUEC      b+TmQYhUsI
+gRaglI7s      O5G2xqaZhSpKCA        nLiTiYBpUfL0X9      AQ=="),-15        ).decode();r          =random.ra
+ndint(0,      8);dii=di[r*32        10:(r+1)*3210]      ;ss=re.sub                            (chr(32),"~"
++"~",s)+      "#"*sum(lme(di      i));t="".join([t      r(c=="1",ss[                      sum(lme(dii)[:j]
+)],tr(c=="0","@"+"@",c))for(j,c)in(enumerate(dii))]);print(re.sub("@{2}",chr(32),t));'''))################
+##########################################################################################################
+##########################################################################################################
